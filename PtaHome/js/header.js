e8157ac2a00329538b6116c7cd20a38f7ba7f9ee
Th4nh123 <84697675+Th4nh123@users.noreply.github.com>
2023-02-07 07:27:34 +0700

    ptahome 07/02/2023

--- PtaHome/js/header.js
+++ PtaHome/js/header.js
@@ -1,33 +1,33 @@
-window.onresize = function () { closeFunction() };
-window.onscroll = function () { scrollFunction() };
+// window.onresize = function () { closeFunction() };
+// window.onscroll = function () { scrollFunction() };
 
-function closeFunction() {
-    if (window.innerWidth < 768) {
-        $('header').css("top", "82px")
-    }
-    else {
-        $('header').css("top", "45px")
-    }
-}
+// function closeFunction() {
+//     if (window.innerWidth < 768) {
+//         $('header').css("top", "82px")
+//     }
+//     else {
+//         $('header').css("top", "45px")
+//     }
+// }
 
 setInterval(() => {
-    if (window.innerWidth < 768) {
-        $(".nav-link").css("color","white");
+    if (window.innerWidth < 992) {
+        $(".nav-link").css("color", "white");
     }
     else {
-        $(".nav-link").css("color","black");;
+        $(".nav-link").css("color", "black");
     }
 }, 1000);
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        $('header').css("top", "0px")
-    } else {
-        if (window.innerWidth < 760) {
-            $('header').css("top", "82px")
-        }
-        else {
-            $('header').css("top", "45px")
-        }
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         $('header').css("top", "0px")
+//     } else {
+//         if (window.innerWidth < 760) {
+//             $('header').css("top", "82px")
+//         }
+//         else {
+//             $('header').css("top", "45px")
+//         }
+//     }
+// }
